84ef1c855e035ad6a5e687b21b2e30260c6669da
RBorn02 <52722128+RBorn02@users.noreply.github.com>
2021-09-16 20:16:24 +0200

    Update builder.py

--- builder.py
+++ builder.py
@@ -1,8 +1,9 @@
 
+
 from models import ViT_Breg, Swin_Breg
 
 class Builder():
-    def __init__(self, base_model, img_size, breg_dim, num_classes,
+    def __init__(self, base_model, img_size, breg_dim,
                  d_subs, hidden_size, bn, device):
         
        if base_model == "ViT16-S":
@@ -10,7 +11,6 @@ class Builder():
                     breg_dim=breg_dim, 
                     d_subs=d_subs,
                     embed_dim=384,
-                    num_classes=num_classes,
                     hidden_size=hidden_size,
                     bn=bn).to(device)
         
@@ -19,7 +19,6 @@ class Builder():
              self.model = ViT_Breg(img_size=img_size,
                     breg_dim=breg_dim, 
                     d_subs=d_subs,
-                    num_classes=num_classes,
                     hidden_size=hidden_size,
                     bn=bn).to(device)
         
@@ -29,7 +28,6 @@ class Builder():
                     breg_dim=breg_dim, 
                     d_subs=d_subs,
                     embed_dim=384,
-                    num_classes=num_classes,
                     patch_size=32,
                     hidden_size=hidden_size,
                     bn=bn).to(device)
@@ -39,7 +37,6 @@ class Builder():
              self.model = ViT_Breg(img_size=img_size, 
                     breg_dim=breg_dim, 
                     d_subs=d_subs,
-                    num_classes=num_classes,
                     patch_size=32,
                     hidden_size=hidden_size,
                     bn=bn).to(device)
@@ -58,7 +55,6 @@ class Builder():
              self.model = Swin_Breg(img_size=img_size,
                           depths=(2, 2, 18, 2),
                           breg_dim=breg_dim,
-                          num_classes=num_classes,
                           d_subs=d_subs,
                           hidden_size=hidden_size,
                           bn=bn).to(device)
@@ -69,9 +65,7 @@ class Builder():
                           depths=(2, 2, 18, 2),
                           embed_dim=128,
                           num_heads=(4, 8, 16, 32),
-                          mlp_dim=1024,
                           breg_dim=breg_dim,
-                          num_classes=num_classes,
                           d_subs=d_subs,
                           hidden_size=hidden_size,
                           bn=bn).to(device)
